f0065deb9d2d2569106fa95d17dcff639dfa882e
edge2992 <torune.summer@gmail.com>
2024-10-06 00:39:03 +0000

    refactor: delete moved settings

--- old/zsh.d/01-plugins.zsh
+++
@@ -1,9 +0,0 @@
-# load plugins
-zinit load zsh-users/zsh-syntax-highlighting
-zinit load zsh-users/zsh-completions
-zinit load zsh-users/zsh-autosuggestions
-zinit load zdharma/history-search-multi-word # <C-r>
-zinit load wfxr/forgit
-
-zinit ice pick'poetry.zsh'
-zinit light sudosubin/zsh-poetry
