c936b4c652158fc6afb72e406a106689187c49a1
edge2992 <torune.summer@gmail.com>
2025-06-29 22:04:30 +0900

    Add multiple LSP server configurations and tools with Mason

---
+++ private_dot_config/nvim/lua/edgissa/plugins/lsp/lspconfig.lua
@@ -0,0 +1,6 @@
+-- nvim-lspconfig prividing LSP support for Neovim
+-- nvim-lspconfig does not have LSP client implementation, it only provides configuration templates.
+-- LSP client implementation is provided by Neovim itself.
+return {
+  "neovim/nvim-lspconfig",
+}
