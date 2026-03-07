0e534b7b186338d8dc4c340227853471917687af
edge2992 <torune.summer@gmail.com>
2026-03-07 23:08:38 +0900

    chore: add wezterm and update claude plugins

---
+++ private_dot_config/wezterm/wezterm.lua
@@ -0,0 +1,23 @@
+local wezterm = require 'wezterm'
+local config = wezterm.config_builder()
+
+config.font = wezterm.font 'UDEV Gothic NF'
+config.font_size = 14.0
+config.color_scheme = 'Tango Dark'
+config.term = 'xterm-256color'
+config.window_decorations = 'RESIZE'
+config.enable_tab_bar = false
+config.window_background_opacity = 0.95
+config.scrollback_lines = 200
+config.use_ime = true
+config.xim_im_name = 'fcitx'
+
+config.keys = {
+  {
+    key = ' ',
+    mods = 'CTRL',
+    action = wezterm.action.DisableDefaultAssignment,
+  },
+}
+
+return config
