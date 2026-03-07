e41642c4e81136bdeddd2f3012181a1497f9c7db
edge2992 <torune.summer@gmail.com>
2026-03-07 23:25:44 +0900

    chore: add fcitx5 config and update wezterm

--- private_dot_config/wezterm/wezterm.lua
+++ private_dot_config/wezterm/wezterm.lua
@@ -2,8 +2,8 @@ local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
 config.font = wezterm.font 'UDEV Gothic NF'
-config.font_size = 14.0
-config.color_scheme = 'Tango Dark'
+config.font_size = 12.0
+config.color_scheme = 'Tokyo Night'
 config.term = 'xterm-256color'
 config.window_decorations = 'RESIZE'
 config.enable_tab_bar = false
@@ -11,6 +11,7 @@ config.window_background_opacity = 0.95
 config.scrollback_lines = 200
 config.use_ime = true
 config.xim_im_name = 'fcitx'
+config.ime_preedit_rendering = 'Builtin'
 
 config.keys = {
   {
