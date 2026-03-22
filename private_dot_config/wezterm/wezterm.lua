ac8cda34f48b0bd4ad1149bad1fa2ccecac001e2
edge2992 <torune.summer@gmail.com>
2026-03-22 13:34:20 +0900

    style(wezterm): normalize quotes to double quotes

--- private_dot_config/wezterm/wezterm.lua
+++ private_dot_config/wezterm/wezterm.lua
@@ -1,22 +1,22 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
-config.font = wezterm.font 'UDEV Gothic NF'
+config.font = wezterm.font("UDEV Gothic NF")
 config.font_size = 12.0
-config.color_scheme = 'Tokyo Night'
-config.term = 'xterm-256color'
-config.window_decorations = 'RESIZE'
+config.color_scheme = "Tokyo Night"
+config.term = "xterm-256color"
+config.window_decorations = "RESIZE"
 config.enable_tab_bar = false
 config.window_background_opacity = 0.95
 config.scrollback_lines = 200
 config.use_ime = true
-config.xim_im_name = 'fcitx'
-config.ime_preedit_rendering = 'Builtin'
+config.xim_im_name = "fcitx"
+config.ime_preedit_rendering = "Builtin"
 
 config.keys = {
   {
-    key = ' ',
-    mods = 'CTRL',
+    key = " ",
+    mods = "CTRL",
     action = wezterm.action.DisableDefaultAssignment,
   },
 }
